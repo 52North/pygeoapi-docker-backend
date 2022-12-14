1784a6e93bdf7d0bb51db76aa4b0e3a881510d36
Alexander Pilz <51150328+xcomagent95@users.noreply.github.com>
2022-12-14 09:42:32 +0100

    some comments

--- process.py
+++ process.py
@@ -1,7 +1,7 @@
 import os
 
-id = os.environ['id']
+id = os.environ['id'] #read evironment variable
 
-fp = open('usr/src/process/data/' + id +'.txt', 'w')
-fp.write('id of the process: ' + str(id))
-fp.close()
+fp = open('usr/src/process/data/' + id +'.txt', 'w') #create result file
+fp.write('id of the process: ' + str(id)) #write result
+fp.close() #close result file
