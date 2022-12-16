989c702f6a44cc520434522023322f0b2469d62f
Alexander Pilz <51150328+xcomagent95@users.noreply.github.com>
2022-12-16 13:32:37 +0100

    changes

--- process/dockerTest.py
+++ process/dockerTest.py
@@ -1,9 +1,10 @@
 import logging
-import math
-import time 
 from random import *
-from pygeoapi.process.base import BaseProcessor, ProcessorExecuteError
+from pygeoapi.process.base import BaseProcessor
 import docker
+import traceback
+import uuid
+import json
 
 
 LOGGER = logging.getLogger(__name__)
@@ -11,14 +12,12 @@ LOGGER = logging.getLogger(__name__)
 #: Process metadata and description
 PROCESS_METADATA = {
     'version': '0.0.1',
-    'id': 'docker_process',
+    'id': 'dockerTest',
     'title': {
-        'en': 'test',
-        'fr': 'test'
+        'en': 'dockerTest'
     },
     'description': {
-        'en': 'Test process for testing docker in conjuction with pygeoapi',
-        'fr': 'Test process for testing docker in conjuction with pygeoapi',
+        'en': 'Test process for testing docker in conjuction with pygeoapi'
     },
     'keywords': ['docker', 'test'],
     'links': [{
@@ -37,8 +36,19 @@ PROCESS_METADATA = {
             },
             'minOccurs': 1,
             'maxOccurs': 1,
-            'metadata': None,  # TODO how to use?
+            'metadata': None,
             'keywords': ['test']
+        },
+        'directory': {
+            'title': 'directory',
+            'description': 'Directory to be mounted to the container',
+            'schema': {
+                'type': 'String'
+            },
+            'minOccurs': 1,
+            'maxOccurs': 1,
+            'metadata': None, 
+            'keywords': ['directory', 'mount']
         }
     },
     'outputs': {
@@ -53,7 +63,8 @@ PROCESS_METADATA = {
     },
     'example': {
         'inputs': {
-            'test': 'some value'
+            'test': 'some value',
+            'directory': 'someabsolutepath'
         }
     }
 }
@@ -70,32 +81,43 @@ class DockerTestProcessor(BaseProcessor):
 
         :returns: pygeoapi.process.test.TestProcessor
         """
-
+        self.id =  str(uuid.uuid4()) #generate job id
         super().__init__(processor_def, PROCESS_METADATA)
 
     def execute(self, data):
         
         mimetype = 'application/json'
 
-        test = data.get('test', None)
-        '''
-        if test is None:
-            raise ProcessorExecuteError('Cannot process without both points')
+        #define evironment variables
+        environment = ["id="+self.id] 
 
-        #Simulate processing time
-        sleepingTime = 10 #randint(5, 60)
-        time.sleep(sleepingTime) 
-        '''
-        outputs = {
-            'id': 'Test',
-            #'slept for ': sleepingTime,
-            'value': test
-        }
-        
-        client = docker.from_env()
-        print(client)
+        client = docker.from_env() #initialize docker client
+        print("RIESGOS - Docker-Client:" , client)
+        try:
+            directory = data["directory"]
+            print("RIESGOS - Results-Directory: ", directory)
+            container = client.containers.run("lexal95/dockerprocess", "sleep infinity", 
+            detach=True, 
+            environment=environment, #add environmet variables
+            volumes={directory: {'bind': '/usr/src/process/data', 'mode': 'rw'}}) #mount specified directory
+            try: 
+                #run commands in container
+                container.exec_run('/bin/sh') #start shell
+                test = container.exec_run('python3 usr/src/process/process.py', detach=False) #execute process
+                container.stop() #stop container
+                container.remove() #remove container
+            except Exception:
+                print("RIESGOS - Process could not be started!")
+                traceback.print_exc()
+        except Exception: 
+            print("RIESGOS - Docker-Container could not be started!")
+            traceback.print_exc()
+        #genrate output of pygeoapi
+        result = open(directory + self.id + 'json',)
+        dataResult = json.load(result)
+        outputs = dataResult
 
-        return mimetype, outputs
+        return mimetype, outputs #return output
 
     def __repr__(self):
         return '<DockerTestProcessor> {}'.format(self.name)
