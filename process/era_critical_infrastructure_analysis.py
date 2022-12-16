1b410b30f70ed80249a62d37706f7b65ab2882b3
Alexander Pilz <51150328+xcomagent95@users.noreply.github.com>
2022-12-16 15:26:34 +0100

    changes

---
+++ process/era_critical_infrastructure_analysis.py
@@ -0,0 +1,146 @@
+import logging
+from random import *
+from pygeoapi.process.base import BaseProcessor
+import docker
+import traceback
+import uuid
+import json
+
+
+LOGGER = logging.getLogger(__name__)
+
+#: Process metadata and description
+PROCESS_METADATA = {
+    'version': '0.0.1',
+    'id': 'era_critical_infrastructure_analysis',
+    'title': {
+        'en': 'dockerTest'
+    },
+    'description': {
+        'en': 'Test process for testing docker in conjuction with pygeoapi'
+    },
+    'keywords': ['docker', 'test'],
+    'links': [{
+        'type': 'text/html',
+        'rel': 'about',
+        'title': 'information',
+        'href': 'https://example.org/process',
+        'hreflang': 'en-US'
+    }],
+    'inputs': {
+        'hazard': {
+            'title': 'hazard',
+            'description': 'hazard to analyse',
+            'schema': {
+                'type': 'String'
+            },
+            'minOccurs': 1,
+            'maxOccurs': 1,
+            'metadata': None,
+            'keywords': ['hazard']
+        },
+        'country': {
+            'title': 'country',
+            'description': 'country to analyse',
+            'schema': {
+                'type': 'String'
+            },
+            'minOccurs': 1,
+            'maxOccurs': 1,
+            'metadata': None,
+            'keywords': ['country']
+        },
+        'intensity': {
+            'title': 'intensity',
+            'description': 'absolute path to the intensity.xml',
+            'schema': {
+                'type': 'String'
+            },
+            'minOccurs': 1,
+            'maxOccurs': 1,
+            'metadata': None,
+            'keywords': ['intensity', 'mount']
+        },
+        'directory': {
+            'title': 'directory',
+            'description': 'Directory to be mounted to the container',
+            'schema': {
+                'type': 'String'
+            },
+            'minOccurs': 1,
+            'maxOccurs': 1,
+            'metadata': None, 
+            'keywords': ['directory', 'mount']
+        }
+    },
+    'outputs': {
+        'result': {
+            'title': 'Result',
+            'description': 'the result of the test',
+            'schema': {
+                'type': 'object',
+                'contentMediaType': 'application/json'
+            }
+        }
+    },
+    'example': {
+        'inputs': {
+            'test': 'some value',
+            'directory': 'someabsolutepath'
+        }
+    }
+}
+
+
+class ciaProcessor(BaseProcessor):
+    """Test Processor example"""
+
+    def __init__(self, processor_def):
+        """
+        Initialize object
+
+        :param processor_def: provider definition
+
+        :returns: pygeoapi.process.test.TestProcessor
+        """
+        self.id =  str(uuid.uuid4()) #generate job id
+        super().__init__(processor_def, PROCESS_METADATA)
+
+    def execute(self, data):
+        
+        mimetype = 'application/json'
+
+        #define evironment variables
+        environment = ["id="+self.id, "hazard="+data["hazard"], "country="+data["country"]] 
+
+        client = docker.from_env() #initialize docker client
+        print("RIESGOS - Docker-Client:" , client)
+        try:
+            resultDirectory = data["directory"]
+            inputDirectory = data["intensity"]
+            print("RIESGOS - Results-Directory: ", resultDirectory)
+            container = client.containers.run("tum_era_cia", "sleep infinity", 
+            detach=True, 
+            environment=environment, #add environmet variables
+            volumes={resultDirectory: {'bind': '/usr/share/git/system_reliability/outputs', 'mode': 'rw'}, inputDirectory: {'bind': '/usr/share/git/system_reliability/inputs', 'mode': 'rw'}}) #mount specified directory
+            try: 
+                #run commands in container
+                container.exec_run('/bin/sh') #start shell
+                #test = container.exec_run('python3 usr/src/process/process.py', detach=False) #execute process
+                #container.stop() #stop container
+                #container.remove() #remove container
+            except Exception:
+                print("RIESGOS - Process could not be started!")
+                traceback.print_exc()
+        except Exception: 
+            print("RIESGOS - Docker-Container could not be started!")
+            traceback.print_exc()
+        #genrate output of pygeoapi
+        result = open(resultDirectory + self.id + 'json',)
+        dataResult = json.load(result)
+        outputs = dataResult
+
+        return mimetype, outputs #return output
+
+    def __repr__(self):
+        return '<ciaProcessor> {}'.format(self.name)
