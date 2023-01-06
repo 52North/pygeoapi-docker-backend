0eb0327b4355839173bc7390b2b19ee0b04c1843
Alexander Pilz <51150328+xcomagent95@users.noreply.github.com>
2023-01-06 10:40:26 +0100

    Delete process.py

--- process.py
+++
@@ -1,9 +0,0 @@
-import os
-
-id = os.environ['id'] #read evironment variable
-
-output = {'processID': id,
-'message': 'docker process done...'}
-fp = open('usr/src/process/data/' + id +'.json', 'w') #create result file
-fp.write(str(output)) #write result
-fp.close() #close result file
